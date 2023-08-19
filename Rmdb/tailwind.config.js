9a868d3f7bdbde7a0732b425628ef5b7e3ca870d
Mhamad6000 <mhamadothman6000@gmail.com>
2023-08-19 20:57:29 +0300

    done

---
+++ Rmdb/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customBlack: "rgb(0,0,0)",
+        customWhite: "#fcf7f7",
+        customBlue: "#128bb5",
+        customYellow: "#deb522",
+        customGray: "#121212",
+      },
+      height: {
+        128: "32rem",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
